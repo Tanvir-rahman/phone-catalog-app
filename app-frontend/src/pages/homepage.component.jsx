6675a7dda1857fd8de603a084b21c0dd42d3dda4
Tanvir Rahman <rahmantanvir25558@gmail.com>
2022-02-22 22:41:29 +0600

    initial setup done

---
+++ app-frontend/src/pages/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1 className="text-red">Homepage</h1>
+    </div>
+  );
+};
+
+export default Homepage;
