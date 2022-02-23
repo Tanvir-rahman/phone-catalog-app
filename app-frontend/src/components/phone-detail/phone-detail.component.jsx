d7fc38cef843535469daeda75f030232a6288ee7
Tanvir Rahman <rahmantanvir25558@gmail.com>
2022-02-23 18:50:26 +0600

    details ui done

--- app-frontend/src/components/phone-detail/phone-detail.component.jsx
+++ app-frontend/src/components/phone-detail/phone-detail.component.jsx
@@ -8,17 +8,49 @@ import './phone-detail.styles.scss';
 
 export const PhoneDetailComponent = ({ details, history }) => {
   return (
-    <div className="phone">
-      {
-        details ? details.title : null
-      }
-    </div>
+    details && (<div className='phone-details'>
+      <div className='title'>Details</div>
+      <div className='details-info'>
+        <div className='details-info-img'>
+          <img
+            src={require('../../assets/images/' + details.imageurl)}
+            alt={details.title}
+          />
+        </div>
+        <div className='details-info-description'>
+          <p>
+            <span>Title: </span>
+            {details.title}
+          </p>
+          <p>
+            <span>Screen: </span>
+            {details.screen}
+          </p>
+          <p>
+            <span>Capacity: </span>
+            {details.capacity}
+          </p>
+          <p>
+            <span>Ram: </span>
+            {details.ram}
+          </p>
+          <p>
+            <span>Color: </span>
+            {details.color}
+          </p>
+          <p>
+            <span>Price: </span>
+            {details.price}
+          </p>
+          <p>{details.description}</p>
+        </div>
+      </div>
+    </div>)
   );
-}
-
+};
 
 const mapStateToProps = createStructuredSelector({
-  details: phoneDetails
+  details: phoneDetails,
 });
 
 export default connect(mapStateToProps)(PhoneDetailComponent);
